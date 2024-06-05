494963bcbf3fad0d3e180648493a78ddba797b48
JosafatMv <20213tn124@utez.edu.mx>
2024-06-05 14:56:16 -0600

    [ADD] Auth methods

--- auth/login/app.py
+++ auth/login/app.py
@@ -1,19 +1,47 @@
 import json
-
-# import requests
+import pymysql
 
 
 def lambda_handler(event, context):
-    """
-        :param event:
-        :param context:
-        :return:
-    """
+    username = event.get('email')
+    password = event.get('password')
 
-    return {
-        "statusCode": 200,
-        "body": json.dumps({
-            "message": "login Lambda",
-            # "location": ip.text.replace("\n", "")
-        }),
-    }
+    if not username or not password:
+        return {
+            'statusCode': 400,
+            'body': json.dumps('Faltan datos para el login.')
+        }
+
+    connection = connect_to_db()
+    if connection is None:
+        return {
+            'statusCode': 500,
+            'body': json.dumps('No se pudo conectar a la base de datos.')
+        }
+
+    try:
+        with connection.cursor() as cursor:
+            sql = "SELECT * FROM users WHERE username = %s AND password = %s"
+            cursor.execute(sql, (username, password))
+            result = cursor.fetchone()
+
+            if result:
+                return {
+                    'statusCode': 200,
+                    'body': json.dumps('Login exitoso.')
+                }
+            else:
+                return {
+                    'statusCode': 401,
+                    'body': json.dumps('Credenciales incorrectas. Verifica tu email y contraseña.')
+                }
+
+    except pymysql.MySQLError as e:
+        print(f"ERROR: {e}")
+        return {
+            'statusCode': 500,
+            'body': json.dumps('Error en el servidor al intentar el login.')
+        }
+
+    finally:
+        connection.close()
