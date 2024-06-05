437e8b07a4392c51502875fadd0a6a2a9c699a4e
DavidRos1231 <20213tn038@utez.edu.mx>
2024-06-05 12:01:38 -0600

    [ADD] Se cambio la database

--- utils.py
+++ utils.py
@@ -5,7 +5,7 @@ def db_connection():
     connection = pymysql.connect(host='databaseforlambdas.czssy4oigfcr.us-east-1.rds.amazonaws.com',
                                  user='admin',
                                  password='admin123',
-                                 database='utez',
+                                 database='chomfit',
                                  charset='utf8mb4',
                                  cursorclass=pymysql.cursors.DictCursor)
     return connection
