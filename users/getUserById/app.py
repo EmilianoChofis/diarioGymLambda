0e398bffc6345919b27f3d35a860b00243f497c3
DavidRos1231 <20213tn038@utez.edu.mx>
2024-06-05 11:41:51 -0600

    [ADD] Se agregó la consulta de usuarios por id y el utils con la conexión a mysql

--- users/getUserById/app.py
+++ users/getUserById/app.py
@@ -1,18 +1,44 @@
 import json
 
+from utils import db_connection
+
+
 # import requests
 
 
 def lambda_handler(event, context):
+
     """
-        :param event:
-        :param context:
-        :return:
+    :param event:
+    :param context:
+    :return:
     """
-    return {
-        "statusCode": 200,
-        "body": json.dumps({
-            "message": "getUserById Lambda",
-            # "location": ip.text.replace("\n", "")
-        }),
-    }
+    id = event['id']
+    connection = db_connection()
+
+    try:
+        with connection.cursor() as cursor:
+            sql = f"select * from users WHERE id = {id}"
+            cursor.execute(sql, (id))
+            result = cursor.fetchone()
+            connection.commit()
+            if result:
+                response = {
+                    'statusCode': 200,
+                    'body': json.dumps(result)
+                }
+            else:
+                response = {
+                    'statusCode': 404,
+                    'body': json.dumps({'message': 'User not found'})
+                }
+
+            return response
+    except Exception as e:
+        return {
+            'statusCode': 500,
+            'body': json.dumps({'message': str(e)})
+        }
+
+    finally:
+        connection.close()
