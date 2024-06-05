2781ebe1a168a226500f800804aceb5398caddd0
DavidRos1231 <20213tn038@utez.edu.mx>
2024-06-05 12:18:37 -0600

    CHOM-6 disable y enableUser

--- users/disableUser/app.py
+++ users/disableUser/app.py
@@ -1,19 +1,36 @@
 import json
 
+from utils import db_connection
+
+
 # import requests
 
 
 def lambda_handler(event, context):
-    """
-        :param event:
-        :param context:
-        :return:
-    """
 
-    return {
-        "statusCode": 200,
-        "body": json.dumps({
-            "message": "disableUser Lambda",
-            # "location": ip.text.replace("\n", "")
-        }),
-    }
+    """
+    :param event:
+    :param context:
+    :return:
+    """
+    id = event['id']
+    connection = db_connection()
+
+    try:
+        with connection.cursor() as cursor:
+            sql = f"UPDATE users SET expire_at = CURRENT_TIMESTAMP, enable = '0' WHERE id = {id}";
+            cursor.execute(sql, (id))
+            connection.commit()
+            response = {
+                'statusCode': 200,
+                'body': json.dumps({'message': 'User was disabled successfully'})
+            }
+            return response
+    except Exception as e:
+        return {
+            'statusCode': 500,
+            'body': json.dumps({'message': str(e)})
+        }
+
+    finally:
+        connection.close()
