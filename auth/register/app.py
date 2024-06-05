5f2e9da7cdc2c181f760defdcf7ca1462a6e7fe9
JosafatMv <20213tn124@utez.edu.mx>
2024-06-05 15:37:01 -0600

    register funcional

--- auth/register/app.py
+++ auth/register/app.py
@@ -36,7 +36,7 @@ def lambda_handler(event, context):
                 }
 
             # Insertar nuevo usuario
-            sql = "INSERT INTO users (username, password, email, role) VALUES (%s, %s, %s, %s)"
+            sql = "INSERT INTO users (username, password, email, role_id) VALUES (%s, %s, %s, %s)"
             cursor.execute(sql, (username, password, email, role))
             connection.commit()
 
