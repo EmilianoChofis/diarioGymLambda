8449a2dd70ce9ae4ba714c7a6b2bec8d93fc415d
JonathanOcampo05 <20233tn116@utez.edu.mx>
2024-06-20 20:35:25 -0600

    prueba incompleta

--- tests/test_app_login.py
+++ tests/test_app_login.py
@@ -1,8 +1,12 @@
 import unittest
 import json
+from unittest.mock import patch
 
+import pymysql
 from botocore.exceptions import ClientError
+from pymysql import MySQLError
 
+import auth.login.app
 from auth.login import app
 
 mock_body = {
@@ -18,10 +22,13 @@ class TestAppLogin(unittest.TestCase):
         result = app.lambda_handler(mock_body, None)
         self.assertEqual(result['statusCode'], 200)
 
-    def test_lambda_fail(self, mock_body):
-        mock_body.side_effect = ClientError(error_response={'error': {'msj': 'error 500'}}, operation_name="login")
+    @patch.dict("os.environ", {"REGION_NAME": "us-east-2", "DATA_BASE": "bd"})
+    @patch("auth.login.app")
+    @patch("pymysql.connect")
+    def test_lambda_fail(self, mock_connect, __):
+        mock_connect.side_effect = ClientError('Error en el servidor al intentar el login.')
         result = app.lambda_handler(mock_body, None)
         self.assertEqual(result['statusCode'], 500)
         body = json.loads(result['body'])
         self.assertIn("error", body)
-        self.assertEqual(body['error'], 'An error ocurred')
+        self.assertEqual(body['error'], 'Error en el servidor al intentar el login.')
