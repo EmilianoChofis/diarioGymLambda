a24664a8a74f825168d8581c98e75261d3d04afa
EmilianoChofis <20213tn128@utez.edu.mx>
2024-06-28 16:28:35 -0600

    regresamos el codigo a su original

--- users/disableUser/app.py
+++ users/disableUser/app.py
@@ -5,15 +5,6 @@ import jwt
 
 def lambda_handler(event, __):
     body = event.get('body')
-    headers = event.get('headers')
-
-    if headers is None:
-        return {
-            'statusCode': 400,
-            'body': json.dumps({
-                "message": "Los headers son requeridos para la petición."
-            })
-        }
 
     if not body:
         return {
@@ -22,24 +13,6 @@ def lambda_handler(event, __):
                 "message": "El body es requerido para la petición."
             })
         }
-
-    data = json.loads(headers)
-    token = data.get('Authorization')
-    claims = jwt.decode(token, options={"verify_signature": False})
-    role = None
-
-    if 'cognito:groups' in claims:
-        role = claims['cognito:groups']
-
-    if role is None or role[0] != 'administradores':
-        return {
-            'statusCode': 401,
-            'body': json.dumps({
-                "message": "No tienes permisos para realizar esta acción."
-            })
-        }
-
-
 
     data = json.loads(body)
 
