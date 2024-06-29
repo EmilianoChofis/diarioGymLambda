bcf4a5e38eb5aa4d02bbe1089e5ac6f080bccc35
JosafatMv <20213tn124@utez.edu.mx>
2024-06-28 18:27:58 -0600

    otro push

--- users/disableUser/app.py
+++ users/disableUser/app.py
@@ -4,90 +4,107 @@ import jwt
 
 
 def lambda_handler(event, __):
-    body = event.get('body')
+    try:
+        body = event.get('body')
+        headers = event.get('headers')
 
-    token = event.get('headers')
-    data = json.loads(token)
-    access_token = data.get('Authorization')
+        if headers is None:
+            return {
+                'statusCode': 401,
+                'body': json.dumps({
+                    "message": "No tienes permisos para realizar esta acción."
+                })
+            }
 
-    if not access_token:
-        return {
-            'statusCode': 401,
-            'body': json.dumps({
-                "message": "No tienes permisos para realizar esta acción."
-            })
-        }
+        access_token = headers.get('Authorization')
 
-    claims = jwt.decode(access_token, options={"verify_signature": False})
-    print(claims)
-    #admin
-    role = None
-    if 'cognito:groups' in claims:
-        role = claims['cognito:groups']
+        if not access_token or not access_token.startswith("Bearer "):
+            return {
+                'statusCode': 401,
+                'body': json.dumps({
+                    "message": "No tienes permisos para realizar esta acción."
+                })
+            }
+
+        access_token = access_token.split(" ")[1]
+
+        claims = jwt.decode(access_token, options={"verify_signature": False})
+        print(claims)
+
+        # Validar rol
+        role = claims.get('cognito:groups')
         print(role)
 
-    if role is None or role[0] != 'administradores':
-        return {
-            'statusCode': 401,
-            'body': json.dumps({
-                "message": "No tienes permisos para realizar esta acción."
-            })
-        }
-
-    if not body:
-        return {
-            'statusCode': 400,
-            'body': json.dumps({
-                "message": "El body es requerido para la petición."
-            })
-        }
-
-    data = json.loads(body)
-
-    userId = data.get('id')
-
-    if not userId:
-        return {
-            'statusCode': 400,
-            'body': json.dumps({
-                "message": "El campo id es requerido."
-            })
-        }
-
-    if not isinstance(userId, int):
-        return {
-            'statusCode': 400,
-            'body': json.dumps({
-                "message": "El campo id debe ser un número."
-            })
-        }
-
-    connection = connect_to_db()
-    if connection is None:
-        return {
-            'statusCode': 500,
-            'body': json.dumps({
-                "message": "Error de servidor. No se pudo conectar a la base de datos. Inténtalo más tarde."
-            })
-        }
-
-    try:
-        with connection.cursor() as cursor:
-            sql = "UPDATE users SET expire_at = CURRENT_TIMESTAMP, enable = '0' WHERE id = %s"
-            cursor.execute(sql, (userId,))
-            connection.commit()
-            response = {
-                'statusCode': 200,
-                'body': json.dumps({'message': 'Usuario deshabilitado correctamente'})
+        if not role or 'administradores' not in role:
+            return {
+                'statusCode': 401,
+                'body': json.dumps({
+                    "message": "No tienes permisos para realizar esta acción."
+                })
             }
-            return response
+
+        if not body:
+            return {
+                'statusCode': 400,
+                'body': json.dumps({
+                    "message": "El body es requerido para la petición."
+                })
+            }
+
+        data = json.loads(body)
+        userId = data.get('id')
+
+        if not userId:
+            return {
+                'statusCode': 400,
+                'body': json.dumps({
+                    "message": "El campo id es requerido."
+                })
+            }
+
+        if not isinstance(userId, int):
+            return {
+                'statusCode': 400,
+                'body': json.dumps({
+                    "message": "El campo id debe ser un número."
+                })
+            }
+
+        connection = connect_to_db()
+        if connection is None:
+            return {
+                'statusCode': 500,
+                'body': json.dumps({
+                    "message": "Error de servidor. No se pudo conectar a la base de datos. Inténtalo más tarde."
+                })
+            }
+
+        try:
+            with connection.cursor() as cursor:
+                sql = "UPDATE users SET expire_at = CURRENT_TIMESTAMP, enable = '0' WHERE id = %s"
+                cursor.execute(sql, (userId,))
+                connection.commit()
+                response = {
+                    'statusCode': 200,
+                    'body': json.dumps({'message': 'Usuario deshabilitado correctamente'})
+                }
+                return response
+        except Exception as e:
+            print(f"Error al ejecutar la consulta: {e}")
+            return {
+                'statusCode': 500,
+                'body': json.dumps({
+                    'message': "Error de servidor. Vuelve a intentarlo más tarde."
+                })
+            }
+
+        finally:
+            connection.close()
     except Exception as e:
+        print(f"Error en lambda_handler: {e}")
         return {
             'statusCode': 500,
             'body': json.dumps({
-                'message': "Error de servidor. Vuelve a intentarlo más tarde."
+                'message': "Error interno del servidor."
             })
         }
-
-    finally:
-        connection.close()
