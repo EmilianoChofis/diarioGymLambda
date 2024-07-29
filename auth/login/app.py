1b6b24b9468c9e25d02fd4b2778b294f6e780936
EmilianoChofis <20213tn128@utez.edu.mx>
2024-07-29 13:42:58 -0600

    agregado de config cors en la lambda de login

--- auth/login/app.py
+++ auth/login/app.py
@@ -72,7 +72,7 @@ def lambda_handler(event, __):
                 'Access-Control-Allow-Origin': '*',
                 'Access-Control-Allow-Credentials': 'true',
                 'Access-Control-Allow-Headers': 'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token',
-                'Access-Control-Allow-Methods': 'OPTIONS,POST'
+                'Access-Control-Allow-Methods': 'POST'
             },
             'body': json.dumps({"error_message": str(e)})
         }
