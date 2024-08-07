68b79261e2489d5aa0e2bf70f16572e637644a2d
DavidRos1231 <20213tn038@utez.edu.mx>
2024-08-07 01:05:14 -0600

    [UPDATE] Se actualizó el update, register y getAll de ejercicios

--- exercises/register/app.py
+++ exercises/register/app.py
@@ -1,77 +1,154 @@
 import json
 import pymysql
 from db_conn import connect_to_db
+import jwt
 
-def validate_admin(connection, user_id):
-    with connection.cursor() as cursor:
-        sql = "SELECT role_id FROM users_inc WHERE id = %s"
-        cursor.execute(sql, (user_id,))
-        result = cursor.fetchone()
-        if result and result['role_id'] == 'admin':
-            return True
-        return False
 
-def lambda_handler(event, context):
-    body = event.get('body')
-
-    if not body:
-        return {
-            'statusCode': 400,
-            'body': json.dumps({
-                "message": "El body es requerido para la petición."
-            })
-        }
-
-    data = json.loads(body)
-    created_by = data.get('created_by')
-    name = data.get('name')
-    description = data.get('description')
-
-    if not created_by or not name or not description:
-        return {
-            'statusCode': 400,
-            'body': json.dumps({
-                "message": "Los campos created_by, name y description son requeridos."
-            })
-        }
-
-    connection = connect_to_db()
-    if connection is None:
-        return {
-            'statusCode': 500,
-            'body': json.dumps({
-                "message": "Error de servidor. No se pudo conectar a la base de datos. Inténtalo más tarde."
-            })
-        }
-
+def lambda_handler(event, __):
     try:
-        if not validate_admin(connection, created_by):
+        body = event.get('body')
+        headers = event.get('headers')
+        if headers is None:
             return {
-                'statusCode': 403,
+                'statusCode': 401,
+                'headers': {
+                    'Access-Control-Allow-Origin': '*',
+                    'Access-Control-Allow-Credentials': 'true',
+                    'Access-Control-Allow-Headers': 'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token',
+                    'Access-Control-Allow-Methods': 'OPTIONS,POST'
+                },
+                'body': json.dumps({
+                    "message": "No tienes permisos para realizar esta acción."
+                })
+            }
+        access_token = headers.get('Authorization')
+
+        if not access_token or not access_token.startswith("Bearer "):
+            return {
+                'statusCode': 401,
+                'headers': {
+                    'Access-Control-Allow-Origin': '*',
+                    'Access-Control-Allow-Credentials': 'true',
+                    'Access-Control-Allow-Headers': 'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token',
+                    'Access-Control-Allow-Methods': 'OPTIONS,POST'
+                },
                 'body': json.dumps({
                     "message": "No tienes permisos para realizar esta acción."
                 })
             }
 
-        with connection.cursor() as cursor:
-            sql = "INSERT INTO exercises_inc (name, description, created_by) VALUES (%s, %s, %s)"
-            cursor.execute(sql, (name, description, created_by))
-            connection.commit()
+        access_token = access_token.split(" ")[1]
+
+        claims = jwt.decode(access_token, options={"verify_signature": False})
+        print(claims)
+
+        # Validar rol
+        role = claims.get('cognito:groups')
+        print(role)
+
+        if not role or 'Admin' not in role :
             return {
-                'statusCode': 201,
+                'statusCode': 401,
+                'headers': {
+                    'Access-Control-Allow-Origin': '*',
+                    'Access-Control-Allow-Credentials': 'true',
+                    'Access-Control-Allow-Headers': 'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token',
+                    'Access-Control-Allow-Methods': 'OPTIONS,POST'
+                },
                 'body': json.dumps({
-                    "message": "Ejercicio creado exitosamente."
+                    "message": "No tienes permisos para realizar esta acción."
+                })
+            }
+        if not body:
+            return {
+                'statusCode': 400,
+                'headers': {
+                    'Access-Control-Allow-Origin': '*',
+                    'Access-Control-Allow-Credentials': 'true',
+                    'Access-Control-Allow-Headers': 'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token',
+                    'Access-Control-Allow-Methods': 'OPTIONS,POST'
+                },
+                'body': json.dumps({
+                    "message": "El body es requerido para la petición."
+                })
+            }
+        data = json.loads(body)
+        created_by = data.get('created_by')
+        name = data.get('name')
+        description = data.get('description')
+
+        if not created_by or not name or not description:
+            return {
+                'statusCode': 400,
+                'headers': {
+                    'Access-Control-Allow-Origin': '*',
+                    'Access-Control-Allow-Credentials': 'true',
+                    'Access-Control-Allow-Headers': 'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token',
+                    'Access-Control-Allow-Methods': 'OPTIONS,POST'
+                },
+                'body': json.dumps({
+                    "message": "Los campos created_by, name y description son requeridos."
                 })
             }
 
-    except pymysql.MySQLError as e:
-        print(f"ERROR: {e}")
+        connection = connect_to_db()
+        if connection is None:
+            return {
+                'statusCode': 500,
+                'headers': {
+                    'Access-Control-Allow-Origin': '*',
+                    'Access-Control-Allow-Credentials': 'true',
+                    'Access-Control-Allow-Headers': 'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token',
+                    'Access-Control-Allow-Methods': 'OPTIONS,POST'
+                },
+                'body': json.dumps({
+                    "message": "Error de servidor. No se pudo conectar a la base de datos. Inténtalo más tarde."
+                })
+            }
+        try:
+            with connection.cursor() as cursor:
+                sql = "INSERT INTO exercises (name, description) VALUES (%s, %s)"
+                cursor.execute(sql, (name, description))
+                connection.commit()
+                return {
+                    'statusCode': 201,
+                    'headers': {
+                        'Access-Control-Allow-Origin': '*',
+                        'Access-Control-Allow-Credentials': 'true',
+                        'Access-Control-Allow-Headers': 'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token',
+                        'Access-Control-Allow-Methods': 'OPTIONS,POST'
+                    },
+                    'body': json.dumps({
+                        "message": "Ejercicio creado exitosamente."
+                    })
+                }
+        except pymysql.MySQLError as e:
+            print(f"ERROR: {e}")
+            return {
+                'statusCode': 500,
+                'headers': {
+                    'Access-Control-Allow-Origin': '*',
+                    'Access-Control-Allow-Credentials': 'true',
+                    'Access-Control-Allow-Headers': 'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token',
+                    'Access-Control-Allow-Methods': 'OPTIONS,POST'
+                },
+                'body': json.dumps({
+                    'message': "Error de servidor. Vuelve a intentarlo más tarde."
+                })
+            }
+
+        finally:
+            connection.close()
+    except Exception as e:
         return {
             'statusCode': 500,
+            'headers': {
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Credentials': 'true',
+                'Access-Control-Allow-Headers': 'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token',
+                'Access-Control-Allow-Methods': 'OPTIONS,POST'
+            },
             'body': json.dumps({
                 'message': "Error de servidor. Vuelve a intentarlo más tarde."
             })
         }
-
-    finally:
-        connection.close()
