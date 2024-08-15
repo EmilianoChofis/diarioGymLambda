fe7c7a254cc2a0ee1b7008d7db9e06dff7b16550
JosafatMv <20213tn124@utez.edu.mx>
2024-08-15 12:55:52 -0600

    [FIX]  adicion de content type en headers

--- users/getAllUsers/app.py
+++ users/getAllUsers/app.py
@@ -96,6 +96,7 @@ def create_response(status_code, message, data=None):
     response = {
         'statusCode': status_code,
         'headers': {
+            'Content-Type': 'application/json',
             'Access-Control-Allow-Origin': '*',
             'Access-Control-Allow-Credentials': 'true',
             'Access-Control-Allow-Headers': 'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token',
