063847fa5136d134432748f0f680e04f29129b24
CrisSoven <20213tn044@utez.edu.mx>
2024-08-18 23:37:09 -0600

    [ADD] find team by couch id agregado

---
+++ teams/findByCouchId/app.py
@@ -0,0 +1,129 @@
+import json
+import  logging
+from botocore.exceptions import ClientError
+
+from queries import get_team_by_couch_id
+from validate_token import validate_token, validate_user_role
+
+def lamda_handler(event, __):
+    try:
+        claims, error_message = validate_token(event)
+
+        if error_message:
+            return {
+                'statusCode': 401,
+                'headers': {
+                    'Access-Control-Allow-Origin': '*',
+                    'Access-Control-Allow-Credentials': 'true',
+                    'Access-Control-Allow-Headers': 'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token',
+                    'Access-Control-Allow-Methods': 'OPTIONS,POST'
+                },
+                'body': json.dumps({
+                    "message": error_message
+                })
+            }
+
+        if not validate_user_role(claims, ['Couch']):
+            return {
+                'statusCode': 403,
+                'headers': {
+                    'Access-Control-Allow-Origin': '*',
+                    'Access-Control-Allow-Credentials': 'true',
+                    'Access-Control-Allow-Headers': 'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token',
+                    'Access-Control-Allow-Methods': 'OPTIONS,POST'
+                },
+                'body': json.dumps({
+                    "message": "No tienes permisos para realizar esta acción."
+                })
+            }
+
+        body_parameters = json.loads(event["body"])
+
+        if not body_parameters:
+            return {
+                'statusCode': 400,
+                'headers': {
+                    'Access-Control-Allow-Origin': '*',
+                    'Access-Control-Allow-Credentials': 'true',
+                    'Access-Control-Allow-Headers': 'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token',
+                    'Access-Control-Allow-Methods': 'OPTIONS,POST'
+                },
+                'body': json.dumps({
+                    "message": "El body es requerido para la petición."
+                })
+            }
+
+        couch_id = body_parameters.get("couch_id")
+
+        if couch_id is None:
+            return {
+                'statusCode': 400,
+                'headers': {
+                    'Access-Control-Allow-Origin': '*',
+                    'Access-Control-Allow-Credentials': 'true',
+                    'Access-Control-Allow-Headers': 'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token',
+                    'Access-Control-Allow-Methods': 'OPTIONS,POST'
+                },
+                'body': json.dumps({
+                    "message": "El campo 'couch_id' es requerido para la petición."
+                })
+            }
+
+        team = get_team_by_couch_id(couch_id)
+
+        if team is None:
+            return {
+                'statusCode': 404,
+                'headers': {
+                    'Access-Control-Allow-Origin': '*',
+                    'Access-Control-Allow-Credentials': 'true',
+                    'Access-Control-Allow-Headers': 'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token',
+                    'Access-Control-Allow-Methods': 'OPTIONS,POST'
+                },
+                'body': json.dumps({
+                    "message": "No se encontró el equipo."
+                })
+            }
+
+        return {
+            'statusCode': 200,
+            'headers': {
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Credentials': 'true',
+                'Access-Control-Allow-Headers': 'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token',
+                'Access-Control-Allow-Methods': 'OPTIONS,POST'
+            },
+            'body': json.dumps({
+                "message": "Equipo encontrado",
+                "data": team
+            })
+        }
+    except ClientError as e:
+        logging.error(f"ERROR: {e}")
+        return {
+            'statusCode': 500,
+            'headers': {
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Credentials': 'true',
+                'Access-Control-Allow-Headers': 'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token',
+                'Access-Control-Allow-Methods': 'OPTIONS,POST'
+            },
+            'body': json.dumps({
+                'message': f"Error en la conexión. {e}"
+            })
+        }
+
+    except Exception as e:
+        logging.error(f"ERROR: {e}")
+        return {
+            'statusCode': 500,
+            'headers': {
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Credentials': 'true',
+                'Access-Control-Allow-Headers': 'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token',
+                'Access-Control-Allow-Methods': 'OPTIONS,POST'
+            },
+            'body': json.dumps({
+                'message': f"Error inesperado. {e}"
+            })
+        }
---
+++ teams/findByCouchId/queries.py
@@ -0,0 +1,18 @@
+import logging
+import pymysql
+from db_conn import connect_to_db
+
+
+def get_team_by_couch_id(couch_id):
+    connection = connect_to_db()
+    try:
+        with connection.cursor() as cursor:
+            sql = "SELECT * FROM teams WHERE couch_id = %s"
+            cursor.execute(sql, (couch_id,))
+            result = cursor.fetchone()
+            return result
+    except pymysql.MySQLError as e:
+        logging.error(f"ERROR: {e}")
+        raise e
+    finally:
+        connection.close()
