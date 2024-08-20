8a2e3bef9a7d0baeca9c4498950d895fb515bcd6
JosafatMv <20213tn124@utez.edu.mx>
2024-08-20 17:56:37 -0600

    [FIX] error de la tabla

---
+++ routines/findByUser/app.py
@@ -0,0 +1,141 @@
+import json
+import logging
+from botocore.exceptions import ClientError
+
+from queries import get_user_routines, get_routine_exercises, get_user_by_uid
+from validate_token import validate_token, validate_user_role
+
+
+def lambda_handler(event, __):
+
+    try:
+
+        claims, error_message = validate_token(event)
+
+        if error_message:
+            return {
+                'statusCode': 401,
+                'headers': {
+                    'Access-Control-Allow-Origin': '*',
+                    'Access-Control-Allow-Credentials': 'true',
+                    'Access-Control-Allow-Headers': 'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token',
+                    'Access-Control-Allow-Methods': 'OPTIONS,POST'
+                },
+                'body': json.dumps({
+                    "message": error_message
+                })
+            }
+
+        if not validate_user_role(claims, ['Couch', 'Admin', 'User']):
+            return {
+                'statusCode': 403,
+                'headers': {
+                    'Access-Control-Allow-Origin': '*',
+                    'Access-Control-Allow-Credentials': 'true',
+                    'Access-Control-Allow-Headers': 'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token',
+                    'Access-Control-Allow-Methods': 'OPTIONS,POST'
+                },
+                'body': json.dumps({
+                    "message": "No tienes permisos para realizar esta acción."
+                })
+            }
+
+        try:
+            body_parameters = json.loads(event["body"])
+        except Exception:
+            return {
+                'statusCode': 400,
+                'headers': {
+                    'Access-Control-Allow-Origin': '*',
+                    'Access-Control-Allow-Credentials': 'true',
+                    'Access-Control-Allow-Headers': 'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token',
+                    'Access-Control-Allow-Methods': 'OPTIONS,POST'
+                },
+                'body': json.dumps({
+                    "message": "El body es requerido para la petición."
+                })
+            }
+
+        userUid = body_parameters.get("userUid")
+
+        if userUid is None:
+            return {
+                "statusCode": 400,
+                'headers': {
+                    'Access-Control-Allow-Origin': '*',
+                    'Access-Control-Allow-Credentials': 'true',
+                    'Access-Control-Allow-Headers': 'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token',
+                    'Access-Control-Allow-Methods': 'OPTIONS,POST'
+                },
+                "body": json.dumps({"message": "EL campo teamId es requerido."})
+            }
+
+
+
+        user = get_user_by_uid(userUid)
+
+        if user is None:
+            return {
+                'statusCode': 404,
+                'headers': {
+                    'Access-Control-Allow-Origin': '*',
+                    'Access-Control-Allow-Credentials': 'true',
+                    'Access-Control-Allow-Headers': 'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token',
+                    'Access-Control-Allow-Methods': 'OPTIONS,POST'
+                },
+                'body': json.dumps({
+                    "message": "El equipo no tiene usuarios registrados."
+                })
+            }
+
+        userRoutines = get_user_routines (user["uid"])
+
+        if userRoutines is not None:
+            for routine in userRoutines:
+                routineExercises = get_routine_exercises(routine["id"])
+                routine["exercises"] = routineExercises
+                user["routines"] = userRoutines
+
+        return {
+            'statusCode': 200,
+            'headers': {
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Credentials': 'true',
+                'Access-Control-Allow-Headers': 'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token',
+                'Access-Control-Allow-Methods': 'OPTIONS,POST'
+            },
+            'body': json.dumps({
+                "message": "Lista de usuarios encontrados dentro del equipo del couch.",
+                "data": user
+            })
+        }
+
+    except ClientError as e:
+        logging.error(f"ERROR: {e}")
+        return {
+            'statusCode': 400,
+            'headers': {
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Credentials': 'true',
+                'Access-Control-Allow-Headers': 'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token',
+                'Access-Control-Allow-Methods': 'OPTIONS,POST'
+            },
+            'body': json.dumps({
+                'message': f"Error en la conexión. {e}"
+            })
+        }
+
+    except Exception as e:
+        logging.error(f"ERROR: {e}")
+        return {
+            'statusCode': 500,
+            'headers': {
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Credentials': 'true',
+                'Access-Control-Allow-Headers': 'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token',
+                'Access-Control-Allow-Methods': 'OPTIONS,POST'
+            },
+            'body': json.dumps({
+                'message': f"Error de servidor. {e}"
+            })
+        }
---
+++ routines/findByUser/queries.py
@@ -0,0 +1,78 @@
+import logging
+import pymysql
+from db_conn import connect_to_db
+
+def get_team_by_id(teamId):
+    connection = connect_to_db()
+    try:
+        with connection.cursor() as cursor:
+            sql = "SELECT * FROM teams WHERE id = %s"
+            cursor.execute(sql, (teamId,))
+            result = cursor.fetchone()
+
+            return result
+    except pymysql.MySQLError as e:
+        logging.error(f"ERROR GETTING TEAM: {e}")
+        raise e
+    finally:
+        connection.close()
+
+def get_users_from_team(teamId):
+    connection = connect_to_db()
+    try:
+        with connection.cursor() as cursor:
+            sql = "SELECT u.* FROM user_group ug INNER JOIN users u ON ug.user_id = u.uid WHERE ug.team_id = %s"
+            cursor.execute(sql, (teamId,))
+            result = cursor.fetchall()
+
+            return result
+    except pymysql.MySQLError as e:
+        logging.error(f"ERROR GETTING USERS FROM A TEAM: {e}")
+        raise e
+    finally:
+        connection.close()
+
+def get_user_routines(uid):
+    connection = connect_to_db()
+    try:
+        with connection.cursor() as cursor:
+            sql = "SELECT * FROM routine WHERE user_id = %s"
+            cursor.execute(sql, (uid,))
+            result = cursor.fetchall()
+
+            return result
+    except pymysql.MySQLError as e:
+        logging.error(f"ERROR GETTING USERS ROUTINES: {e}")
+        raise e
+    finally:
+        connection.close()
+
+def get_routine_exercises(routineId):
+    connection = connect_to_db()
+    try:
+        with connection.cursor() as cursor:
+            sql = "SELECT * FROM routine_exercise re INNER JOIN exercises e ON e.id = re.exercise_id WHERE re.routine_id = %s"
+            cursor.execute(sql, (routineId,))
+            result = cursor.fetchall()
+
+            return result
+    except pymysql.MySQLError as e:
+        logging.error(f"ERROR GETTING ROUTINE EXERCISES: {e}")
+        raise e
+    finally:
+        connection.close()
+
+def get_user_by_uid(uid):
+    connection = connect_to_db()
+    try:
+        with connection.cursor() as cursor:
+            sql = "SELECT * FROM users WHERE uid = %s"
+            cursor.execute(sql, (uid,))
+            result = cursor.fetchone()
+
+            return result
+    except pymysql.MySQLError as e:
+        logging.error(f"ERROR GETTING USER BY UID: {e}")
+        raise e
+    finally:
+        connection.close()
